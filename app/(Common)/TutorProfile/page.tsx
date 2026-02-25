a798fa14dcc486acc3b9a1581661e0eb9a97e187
Abdullah5-boop <Abdullah5-boop@users.noreply.github.com>
2026-02-25 13:52:17 +0600

    tutor profile added

---
+++ app/(Common)/TutorProfile/page.tsx
@@ -0,0 +1,9 @@
+export default function TutorProfile() {
+    return(<div className=" w-screen p-4 grid grid-cols-4 gap-4">
+        <div className="bg-red-500"></div>
+        <div className="bg-green-500"></div>
+        <div className="bg-blue-500"></div>
+        <div className="bg-yellow-500"></div>
+    </div>
+    )
+}
